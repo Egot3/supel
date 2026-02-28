2d3f8de9320a32918879f3142f2b5f0ddac34c6a
Egot3 <noobicklp@gmail.com>
2026-02-28 23:55:53 +0300

    login page + sveltekit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import '$lib/assets/styles/login.sass'
+</script>
+
+<div style="display: grid; grid-template: repeat(12, 1fr) / repeat(12, 1fr); height: 100vh; width:100vw">
+    <div class="login-header">
+        LOGIN
+    </div>
+    <div class="login-body">
+        <input/>
+        <input/>
+        <button type="submit">
+            SUBMIT
+        </button>
+    </div>
+</div>
