63aa36e211a19e13b11eae2ff770835332c75f59
Egot3 <noobicklp@gmail.com>
2026-03-13 00:18:53 +0300

    feat:New works
    will be testing masonry

--- frontend/src/lib/components/New.svelte
+++ frontend/src/lib/components/New.svelte
@@ -2,7 +2,7 @@
 	const { imageLink = '', caption, text } = $props();
 </script>
 
-/*got the joke? News are not countable*/
+<!-- /*got the joke? News are not countable*/ -->
 <div class="new">
 	{#if imageLink}
 		<img src={imageLink} alt="" class="img" />
@@ -11,6 +11,7 @@
 		<p class="caption">
 			{caption}
 		</p>
+		<br />
 		<div class="text">
 			{text}
 		</div>
@@ -18,22 +19,45 @@
 </div>
 
 <style lang="sass">
+    @use '../assets/_variables' as *
+    @use 'sass:math'
+    .new
+        background: $blacker
+        border: $whiter 0.5rem solid
+        padding: 1rem
+        border-radius: 1rem
+        
     .caption 
         min-height: 1.5rem
         max-height: 5rem
 
     .text
-        min-height: 3rem
+        min-height: 2rem
         max-height: 30rem
+        overflow: scroll
+        -ms-overflow-style: none
+        scrollbar-width: none  //--ux
+        margin-bottom: 0.3rem
+        $start-opaque: 80%
+        mask-image: linear-gradient(to bottom, 
+            black 0%, 
+            black $start-opaque, 
+            transparent math.min(100%, $start-opaque + 20%)
+        )
+    
+    .text::-webkit-scrollbar //PWA
+        display: none
+        width: 0
+        height: 0
 
     .img
         min-height: 4rem
         max-height: 10rem
 
-    .new:nth-child(3n+1)
-        grid-column: 1 / 4
-    .new:nth-child(3n+2)
-        grid-column: 5 / 8
-    .new:nth-child(3n+3)
-        grid-column: 9 / 12
+    // .new:nth-child(3n+1)
+    //     grid-column: 1 / 5
+    // .new:nth-child(3n+2)
+    //     grid-column: 5 / 9
+    // .new:nth-child(3n+3)
+    //     grid-column: 9 / 13
 </style>
--- frontend/src/routes/news/+page.svelte
+++ frontend/src/routes/news/+page.svelte
@@ -1,2 +1,31 @@
 <script lang="ts">
+	import '$lib/assets/styles/news.sass';
+	import New from '$lib/components/New.svelte';
 </script>
+
+<div class="news-header">NEWS</div>
+<div class="news-wrapper">
+	<New
+		caption="1"
+		text="Ex mollit dolor dolor nostrud aliqua. Deserunt velit veniam non anim esse. Velit consectetur consectetur voluptate laboris laborum minim eu eiusmod cillum tempor pariatur ipsum amet velit.
+
+Qui qui deserunt fugiat tempor laboris magna reprehenderit velit officia deserunt officia. Officia cupidatat sint fugiat commodo. Qui consequat magna anim anim ad amet est amet ut occaecat. Occaecat ipsum nulla adipisicing est. Ullamco fugiat veniam ad ad occaecat veniam et ullamco quis. Magna ut reprehenderit non labore occaecat.
+
+Occaecat duis voluptate laboris ut tempor nisi velit. Proident nostrud labore eiusmod commodo do cupidatat ex fugiat eu enim commodo aliquip. Voluptate ipsum nulla consectetur ut nostrud anim culpa exercitation nisi laborum ad exercitation elit anim. Aute et enim velit eu id veniam velit reprehenderit dolor ullamco nostrud aliqua commodo. Voluptate Lorem nulla commodo esse laborum occaecat amet. In sunt enim mollit elit fugiat proident do. Labore eiusmod irure deserunt sint aute ipsum in et consequat.
+
+Mollit occaecat consectetur ullamco aute laboris ut est tempor aliqua Lorem. Sunt commodo cupidatat fugiat est aliquip. Elit veniam sunt sint officia qui adipisicing fugiat est sunt. Culpa qui aute minim ex voluptate magna consequat minim ad. Ullamco irure proident Lorem dolor. Ex deserunt labore non aute minim eiusmod pariatur Lorem consequat ullamco eiusmod esse in aliquip.
+
+Nostrud officia dolor excepteur velit non sint fugiat adipisicing et cupidatat fugiat dolore sit aliqua. Consectetur elit ad tempor enim est amet. Sit elit non commodo fugiat occaecat sit irure ad irure elit mollit enim. Commodo do aliqua et pariatur sint ullamco cillum dolor. Cillum laborum sit incididunt exercitation laborum est ex aute.
+
+Incididunt id mollit magna eu ex in sit ea officia deserunt dolor reprehenderit aute. Commodo exercitation cupidatat exercitation labore nulla magna anim laboris non magna consequat quis. Consequat reprehenderit voluptate labore exercitation ad ipsum non eiusmod esse. Consectetur proident amet commodo esse sunt do.
+
+Qui labore exercitation laboris aute ad in cupidatat sit consequat ad deserunt. Lorem exercitation qui elit eu. Nostrud non minim ea reprehenderit sint.
+
+Nulla ipsum culpa adipisicing et esse elit. Mollit minim tempor non fugiat ipsum Lorem tempor consectetur dolor. Amet reprehenderit id incididunt dolore ipsum anim ut. Sint laborum ea sit labore commodo velit veniam sint reprehenderit. Amet amet consectetur esse adipisicing magna commodo laboris duis exercitation ad. Ad incididunt qui elit deserunt et aute adipisicing in consectetur aliqua velit mollit.
+
+Deserunt reprehenderit non minim laboris do enim anim nulla excepteur. Non Lorem consequat cillum tempor do Lorem velit aliqua occaecat occaecat. Eu laboris anim proident sint culpa velit ut in enim non occaecat consectetur aliqua."
+	/>
+	<New caption="1" text="lorem" />
+	<New caption="1" text="lorem" />
+	<New caption="1" text="lorem" />
+</div>
