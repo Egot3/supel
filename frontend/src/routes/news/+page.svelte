51be352c8e849a1f0cff3dff8f277091b6acefa5
Egot3 <noobicklp@gmail.com>
2026-03-15 01:03:57 +0300

    masonry layout

--- frontend/src/routes/news/+page.svelte
+++ frontend/src/routes/news/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	//import '$lib/assets/styles/news.sass';
 	import New from '$lib/components/New.svelte';
-	import { Gallery } from 'flowbite-svelte';
+	import { idGenerator } from 'flowbite-svelte';
 	const itemsRaw = [
 		{ id: '1', text: '1', caption: '2' },
 		{ id: '2', text: '1', caption: '2' },
@@ -43,6 +43,7 @@
 	const items = $derived(dismantle(itemsRaw, 4));
 </script>
 
+/* eslint-disable svelte/require-each-key */ /* eslint-disable svelte/require-each-key */
 <div
 	class="inline-flex bg-accent items-center justify-center text-5xl font-hollow italic cursor-pointer select-none row-start-1"
 >
@@ -56,7 +57,7 @@
 </div>
 
 <div class="bg-dark col-start-2 col-end-12 row-start-4 row-end-12 overflow-scroll">
-	<Gallery class="grid-cols-2 md:grid-cols-4 gap-4 p-4">
+	<!-- <Gallery class="grid-cols-2 md:grid-cols-4 gap-4 p-4">
 		<Gallery>
 			{#each items[0] as item (item.id)}
 				<New caption={item.caption} text={item.text} />
@@ -77,5 +78,15 @@
 				<New caption={item.caption} text={item.text} />
 			{/each}
 		</Gallery>
-	</Gallery>
+	</Gallery> -->
+
+	<div class="flex flex-row gap-4 p-4">
+		{#each items as column}
+			<div class="flex flex-col gap-4 flex-1">
+				{#each column as item (item.id)}
+					<New caption={item.caption} text={item.text} />
+				{/each}
+			</div>
+		{/each}
+	</div>
 </div>
