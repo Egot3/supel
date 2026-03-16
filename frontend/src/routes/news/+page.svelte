70f59863c9ac6b8100696c4f601f787c517d00c7
Egot3 <noobicklp@gmail.com>
2026-03-16 20:46:28 +0300

    fix: refactored colors

--- frontend/src/routes/news/+page.svelte
+++ frontend/src/routes/news/+page.svelte
@@ -2,8 +2,12 @@
 	//import '$lib/assets/styles/news.sass';
 	import New from '$lib/components/New.svelte';
 	import { withoutAuth } from '$lib/requestUtils/axiosConfigs';
+
 	import axios from 'axios';
-	import { Button, Spinner } from 'flowbite-svelte';
+
+	import { Button, Spinner, Modal } from 'flowbite-svelte';
+	import { MailBoxOutline } from 'flowbite-svelte-icons';
+	import { slide } from 'svelte/transition';
 
 	interface newInterface {
 		id: string;
@@ -89,11 +93,36 @@
 	fetchNextPage();
 
 	const items = $derived(dismantle(itemsRaw, 4));
+
+	let popupModal = $state(false);
 </script>
 
-<!-- <PostAction>
-	POST
-</PostAction> -->
+<Button onclick={() => (popupModal = true)} class="bg-coral-500 col-start-1 text-4xl italic"
+	>New new</Button
+>
+<!-- my genius knows no border-radius -->
+
+<Modal
+	form
+	bind:open={popupModal}
+	size="xs"
+	class="bg-forest-900 dark:bg-forest-900 backdrop:bg-linen-900/50 open:flex flex-col w-full p-gutter rounded-lg divide-y text-linen-200 pointer-events-auto my-auto mx-auto max-w-md"
+	transition={slide}
+	permanent
+>
+	<div class="text-left">
+		<MailBoxOutline class="mx-left mb-4 h-12 w-12 text-light bg-dark" />
+		<h2 class="mb-5 text-lg font-normal text-light bg-dark">New constructor</h2>
+		<div class="space-x-2 bg-dark text-light">
+			<Button
+				type="submit"
+				value="yes"
+				class="text-center font-medium inline-flex items-center justify-center text-linen-200 bg-coral-700 hover:bg-coral-800 dark:bg-coral-600 dark:hover:bg-coral-700 focus-within:ring-coral-300 dark:focus-within:ring-coral-900 px-5 py-2.5 text-sm focus-within:ring-4 focus-within:outline-hidden rounded-lg"
+				color="red">Post now</Button
+			>
+		</div>
+	</div>
+</Modal>
 
 <div
 	class="col-start-3 row-start-1 col-end-11 row-end-3 bg-dark text-[clamp(1rem,10vh,7rem)] inline-grid items-center justify-center italic font-hollow tracking-[0.5rem] select-none pointer-events-none"
