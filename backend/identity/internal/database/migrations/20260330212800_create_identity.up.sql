1e861d8e0d77c52b39866c2d5177673955c041c2
Egot3 <noobicklp@gmail.com>
2026-04-09 00:16:22 +0300

    fix(backend):sql conventions
    they are playing around

--- backend/identity/internal/database/migrations/20260330212800_create_identity.up.sql
+++ backend/identity/internal/database/migrations/20260330212800_create_identity.up.sql
@@ -4,7 +4,7 @@ CREATE TYPE user_role AS ENUM('USER','ADMIN');
 CREATE TABLE IF NOT EXISTS users (
     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     role user_role NOT NULL,
-    is_active BOOLEAN DEFAULT TRUE;
-    email VARCHAR(255) NOT NULL;
-    password_hash VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL;
 );
