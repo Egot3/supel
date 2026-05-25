ed41d712e2912e835d595e5c44ca80c4956781b8
ETS <noobicklp@gmail.com>
2026-05-25 21:32:20 +0300

    feat(backend:timetable): implemented getTimetable

--- backend/timetable/internal/database/repositories/timetable.go
+++ backend/timetable/internal/database/repositories/timetable.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/Egot3/supel/backend/timetable/internal/models"
+	"github.com/Egot3/supel/backend/timetable/internal/types"
 	"github.com/samber/do/v2"
 	"github.com/uptrace/bun"
 )
@@ -23,9 +24,26 @@ func NewTimetableRepository(i do.Injector) (TimetableRepository, error) {
 func (r *bunTimetableRepository) ListTimetable(ctx context.Context, groupUUID string, weekNumber, year int) ([]*models.ConcreteLesson, error) {
 	var lessons []*models.ConcreteLesson
 	err := r.db.NewSelect().Model(&lessons).
-		Where("group_uuid = ?", groupUUID).Where("year = ?", year).Where("week_number = ?", weekNumber).
+		TableExpr("concrete_lessons as cl").
+		Where("group_uuid = ?", groupUUID).Join("JOIN periods AS p ON p.uuid = cl.period_uuid").
+		Where("year = ?", year).Where("week_number = ?", weekNumber).
 		Order("day_of_week ASC").
 		Scan(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return lessons, nil
+}
+
+func (r *bunTimetableRepository) Timetable(ctx context.Context, groupUUID string, day types.Day, weekNumber, year int) ([]*models.ConcreteLesson, error) {
+	var lessons []*models.ConcreteLesson
+	err := r.db.NewSelect().Model(&lessons).
+		TableExpr("concrete_lessons as cl").
+		Where("group_uuid = ?", groupUUID).Join("JOIN periods AS p ON p.uuid = cl.period_uuid").
+		Where("year = ?", year).Where("week_number = ?", weekNumber).Where("day_of_week = ?", day).
+		Order("position ASC").Relation("AbstractLesson")
+	Scan(ctx)
 	if err != nil {
 		return nil, err
 	}
--- backend/timetable/internal/server/timetableServer.go
+++ backend/timetable/internal/server/timetableServer.go
@@ -9,6 +9,7 @@ import (
 )
 
 func (s *TimetableServer) ListTimetable(ctx context.Context, req *ttpb.ListTimetablesRequest) (*ttpb.ListTimetableResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "")
 
 	userGroups, err := s.Client.UsersGroups(ctx, req.UserUuid)
 	if err != nil {
@@ -55,6 +56,40 @@ func (s *TimetableServer) ListTimetable(ctx context.Context, req *ttpb.ListTimet
 	} //something wicked this way comes
 
 	return &ttpb.ListTimetableResponse{
+		Timetable: allLessons,
+	}, nil
+}
+
+func (s *TimetableServer) GetTimetable(ctx context.Context, req *ttpb.GetTimetableRequest) (*ttpb.GetTimetableResponse, error) {
+
+	userGroups, err := s.Client.UsersGroups(ctx, req.UserUuid)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "couldn't retrieve user's groups")
+	}
+
+	allLessons := &ttpb.ConcreteLessonShortEntry{Day: req.Day}
+	allLessons.Day = req.Day
+	for _, group := range userGroups {
+		groupLessons, err := s.timetableRepository.ListTimetable(ctx, group, int(req.WeekNumber), 2026)
+		if err != nil {
+			return nil, status.Error(codes.Internal, "couldn't get group lessons for that week")
+		}
+		for _, lesson := range groupLessons {
+			allLessons.Lessons = append(allLessons.Lessons, &ttpb.ShortConcreteLesson{
+				WeekNumber: uint32(lesson.Period.WeekNumber),
+				Year:       uint32(lesson.Period.Year),
+				Period:     uint32(lesson.Period.Position),
+				AbstractLesson: &ttpb.AbstractLesson{
+					Uuid: lesson.AbstractUUID,
+					Name: lesson.AbstractLesson.Name,
+				},
+				Building:   lesson.Building,
+				Auditorium: lesson.Auditorium,
+			})
+		}
+	} //something wicked this way comes
+
+	return &ttpb.GetTimetableResponse{
 		Timetable: allLessons,
 	}, nil
 }
