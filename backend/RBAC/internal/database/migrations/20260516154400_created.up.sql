ab61dacd5de64b3e7158e66c4175739b63a8c708
ETS <noobicklp@gmail.com>
2026-05-27 18:49:56 +0300

    fix(backend:rbac)

--- backend/RBAC/internal/database/migrations/20260516154400_created.up.sql
+++ backend/RBAC/internal/database/migrations/20260516154400_created.up.sql
@@ -7,16 +7,14 @@ CREATE TYPE IF NOT EXISTS verb AS ENUM (
     'DELETE'
 );
 
-
 CREATE TABLE IF NOT EXISTS "actions" (
     action_uuid UUID              PRIMARY KEY DEFAULT gen_random_uuid(),
     scope       character varying NOT NULL,
     sub_scope   character varying NULL,
     "verb"      verb              NOT NULL
 );
-CREATE UNIQUE INDEX idx_actions_full ON "actions" ("scope", "sub_scope", "verb") WHERE "sub_scope" IS NOT NULL;
-CREATE UNIQUE INDEX idx_actions_subscope_null ON "actions" ("scope", "verb") WHERE "sub_scope" IS NULL;
-
+CREATE UNIQUE INDEX IF NOT EXISTS idx_actions_full ON "actions" ("scope", "sub_scope", "verb") WHERE "sub_scope" IS NOT NULL;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_actions_subscope_null ON "actions" ("scope", "verb") WHERE "sub_scope" IS NULL;
 
 CREATE TABLE IF NOT EXISTS "roles" (
     role_uuid        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -31,16 +29,14 @@ CREATE TABLE IF NOT EXISTS "roles" (
     deleted_at TIMESTAMPTZ NULL,
     updated_at TIMESTAMPTZ DEFAULT now()
 );
-CREATE INDEX idx_role_uuid_active ON roles (role_uuid) WHERE deleted_at IS NULL;
-
+CREATE INDEX IF NOT EXISTS idx_role_uuid_active ON roles (role_uuid) WHERE deleted_at IS NULL;
 
 CREATE TABLE IF NOT EXISTS "roles_actions" (
     action_uuid UUID REFERENCES actions(action_uuid) ON DELETE CASCADE ON UPDATE CASCADE,
     role_uuid   UUID REFERENCES roles(role_uuid) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY(action_uuid, role_uuid)
 );
-CREATE INDEX idx_role_uuid_junction ON "roles_actions" ("role_uuid");
-
+CREATE INDEX IF NOT EXISTS idx_role_uuid_junction ON "roles_actions" ("role_uuid");
 
 CREATE TABLE IF NOT EXISTS "users_roles" (
     user_uuid     UUID NOT NULL, --SAGA
@@ -52,5 +48,4 @@ CREATE TABLE IF NOT EXISTS "users_roles" (
 
     PRIMARY KEY(user_uuid, role_uuid)
 );
-CREATE INDEX idx_user_uuid ON "users_roles" ("user_uuid");
-CREATE INDEX idx_users_roles_active ON users_roles (user_uuid) WHERE expires_at IS NULL OR expires_at > now();
+CREATE INDEX IF NOT EXISTS idx_user_uuid ON "users_roles" ("user_uuid");
